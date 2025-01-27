5f5fe67d91bb54f119d6807cf7258f930d059960
alain <armelgonze@yahoo.fr>
2025-01-28 00:55:02 +0100

    refacto

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gevoul/screens/home/home_screen.dart';
-import 'constants.dart';
+import 'core/constants.dart';
 
 void main() {
   runApp(const MyApp());
